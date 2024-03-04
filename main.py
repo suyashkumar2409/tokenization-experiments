b7098b99256413cd19f08b3af7a27eaab5d2ceeb
Suyash Kumar <suyashkr@Suyashs-MacBook-Air.local>
2024-03-04 09:11:37 -0500

    replicated but has \n bug

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import yaml
 import pickle
+import re
+
 from transformers import AutoTokenizer
 from datasets import load_dataset
 from tokenizers.pre_tokenizers import Sequence
@@ -7,6 +9,7 @@ from tokenizers import NormalizedString, PreTokenizedString
 from tokenizers.pre_tokenizers import PreTokenizer
 from DPSplitter import DPSplitter
 from typing import List
+from bidict import bidict
 
 
 def llama_tokenizer(tokenizer_path):
@@ -50,10 +53,69 @@ def evaluate(original_tokenizer, dp_tokenizer, mbpp_dataset):
         range(len(original_tokens)) if
         len(original_tokens[i]) != len(new_tokens[i])]
 
-    with open('list.pkl', 'wb') as f:
+    with open('list_new.pkl', 'wb') as f:
         pickle.dump(indices_with_difference, f)
 
     print(indices_with_difference)
+
+
+class DPTokenizer:
+    def __init__(self, vocab):
+        self.vocab = vocab
+        self.dp_splitter = DPSplitter(vocab)
+
+    def split_like_llama(self, input):
+        # Pattern to match words or sequences of spaces
+        pattern = re.compile(r'\S+|\s+')
+
+        result = pattern.findall(input)
+
+        SPIECE_UNDERLINE = "▁"
+
+        result = [token[1:] if token[0] == ' ' else token for token in result]
+        result = [token for token in result if len(token) > 0]
+        result = [SPIECE_UNDERLINE + token if not token.isspace() else token for token in result]
+        result = [token.replace(' ', SPIECE_UNDERLINE) for token in result]
+
+        return result
+
+    def encode(self, input):
+        pre_tokens = self.split_like_llama(input)
+        tokens = []
+
+        for pre_token in pre_tokens:
+            dp_splits = self.dp_splitter.split(pre_token)
+            tokens.extend(dp_splits)
+
+        return [self.vocab[token] for token in tokens]
+
+
+def debug(llamatokenizer, dp_tokenizer, vocab):
+    # input = "Hey     This is Suyash"
+    input = """def remove_Occ(s,ch): 
+       for i in range(len(s)): 
+           if (s[i] == ch): 
+               s = s[0 : i] + s[i + 1:] 
+               break
+       for i in range(len(s) - 1,-1,-1):  
+           if (s[i] == ch): 
+               s = s[0 : i] + s[i + 1:] 
+               break
+       return s """
+    #
+    tokens = llamatokenizer.encode(input)
+    print("For Llama tokenizer")
+    for token in tokens:
+        print(f"Original token {token} Decoded form {llamatokenizer.decode(token)}")
+
+    dp_tokens = dp_tokenizer.encode(input)
+
+    print(f"Num tokens in llamatokenizer {len(tokens)} and dp tokenizer {len(dp_tokens)}")
+
+    #
+    # print("For DP tokenizer")
+    # for token in dp_tokens:
+    #     print(f"Original token {token} Decoded form {llamatokenizer.decode(token)}")
 
 
 if __name__ == "__main__":
@@ -64,59 +126,15 @@ if __name__ == "__main__":
 
     llamatokenizer = AutoTokenizer.from_pretrained('/Users/suyashkr/tokenization/llama', use_fast=False,
                                                    add_bos_token=False)
-
-    from bidict import bidict
-
-    input = "Hey"
-    tokens = llamatokenizer.encode(input)
     vocab = llamatokenizer.get_vocab()
 
-    vocab_bidict = bidict(vocab)
-    temp_tokens = llamatokenizer._tokenize("Hey")
+    dp_tokenizer = DPTokenizer(vocab)
 
-    from tokenizers.pre_tokenizers import WhitespaceSplit
+    debug(llama_tokenizer, dp_tokenizer, vocab)
 
-    pre_tokenizer = WhitespaceSplit()
-    white_space_pretokens = pre_tokenizer.pre_tokenize_str("Hey       This is Suyash")
-
-    import re
-
-    # Pattern to match words or sequences of spaces
-    pattern = re.compile(r'\S+|\s+')
-
-    result = pattern.findall(input)
-
-    SPIECE_UNDERLINE = "▁"
-
-    result = [token[1:] if token[0] == ' ' else token for token in result]
-    result = [token for token in result if len(token) > 0]
-    result = [SPIECE_UNDERLINE + token if not token.isspace() else token for token in result]
-    result = [token.replace(' ', SPIECE_UNDERLINE) for token in result]
-
-    print(result)
-
-
-    def tokenize(text):
-        splits = [[l for l in word] for word in text]
-        for pair, merge in vocab.items():
-            for idx, split in enumerate(splits):
-                i = 0
-                while i < len(split) - 1:
-                    if split[i] == pair[0] and split[i + 1] == pair[1]:
-                        split = split[:i] + [merge] + split[i + 2:]
-                    else:
-                        i += 1
-                splits[idx] = split
-
-        return sum(splits, [])
-
-
-    result_tokenize = tokenize(result)
-
-    print(white_space_pretokens)
-
-    for token in tokens:
-        print(f"Original token {token} Decoded form {llamatokenizer.decode(token)}")
+    # mbpp_dataset = load_dataset(config['mbpp_path'], split="test")
+    #
+    # evaluate(llamatokenizer, dp_tokenizer, mbpp_dataset)
 
     # dp_tokenizer = AutoTokenizer.from_pretrained((config['tokenizer_path']))
     # original_pre_tokenizer = dp_tokenizer.backend_tokenizer.pre_tokenizer
@@ -124,6 +142,3 @@ if __name__ == "__main__":
     # dp_tokenizer.backend_tokenizer.pre_tokenizer = Sequence([original_pre_tokenizer, PreTokenizer.custom(
     #     CustomPreTokenizer(vocab=dp_tokenizer.get_vocab().keys()))])
     #
-    # mbpp_dataset = load_dataset(config['mbpp_path'], split="test")
-    #
-    # evaluate(original_tokenizer, dp_tokenizer, mbpp_dataset)
